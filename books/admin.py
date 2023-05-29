9333f881daff00380b1459463318f6220555229b
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-05-29 21:48:29 +0500

    initial commit

---
+++ books/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Book
+
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Book, BookAdmin)
